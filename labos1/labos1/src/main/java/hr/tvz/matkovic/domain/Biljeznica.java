e314e97e2f3e24bc2f6a22ca7e94a03ed979016f
Matkovic123 <toni.matkovic0@gmail.com>
2017-03-28 14:57:14 +0200

    Prvi labos gotov

---
+++ labos1/labos1/src/main/java/hr/tvz/matkovic/domain/Biljeznica.java
@@ -0,0 +1,35 @@
+package hr.tvz.matkovic.domain;
+
+public class Biljeznica {
+
+	private Long id;
+	private String naziv;
+	private String opis;
+	
+	public Biljeznica(Long id, String naziv, String opis) {
+		super();
+		this.id = id;
+		this.naziv = naziv;
+		this.opis = opis;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getOpis() {
+		return opis;
+	}
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+	
+	
+}
